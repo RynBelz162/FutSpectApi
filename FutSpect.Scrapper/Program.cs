e7b489b3884a03a2b719f9e3ebd0620f358ca98b
RynBelz162 <rynbelz162@gmail.com>
2025-02-26 20:13:15 -0500

    🎉 add projects

---
+++ FutSpect.Scrapper/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://playwright.dev/dotnet");
+
+Console.ReadLine();
