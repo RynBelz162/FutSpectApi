558597bf7acb88041d657d1fad60c4703c8cd486
RynBelz162 <rynbelz162@gmail.com>
2025-02-28 20:53:56 -0500

    ♻️ convert to host pattern

--- FutSpect.Scrapper/Program.cs
+++ FutSpect.Scrapper/Program.cs
@@ -1,37 +1,11 @@
-﻿using FutSpect.Scrapper.Services;
-using FutSpect.Shared.Models;
-using Microsoft.Playwright;
+﻿using FutSpect.Scrapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-using var playwright = await Playwright.CreateAsync();
-await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
-var page = await browser.NewPageAsync();
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<ScrapperService>();
 
-await page.GotoAsync("https://mlssoccer.com/clubs");
-
-var clubLogos = await page.Locator(".mls-o-clubs-hub-clubs-list__club-logo").AllAsync();
-
-var clubInfoTasks = clubLogos.Select(GetClubInfo);
-
-var results = await Task.WhenAll(clubInfoTasks);
-var clubInfo = results
-    .Where(x => x is not null)
-    .ToList();
+IHost host = builder.Build();
+host.Run();
 
 Console.ReadLine();
-
-static async Task<ClubInfo?> GetClubInfo(ILocator locator)
-{
-    var imageElement = locator.Locator("picture").Locator("img");
-    var image = await imageElement.GetAttributeAsync("src");
-    var name = await imageElement.GetAttributeAsync("alt");
-
-    if (string.IsNullOrWhiteSpace(image) || string.IsNullOrWhiteSpace(name))
-    {
-        return null;
-    }
-
-    var (Bytes, Extension) = await ImageDownloaderService.DownloadImageAsync(image);
-
-    var clubLogo = new ClubLogo(image, Bytes, Extension);
-    return new ClubInfo(name, clubLogo);
-}
---
+++ FutSpect.Scrapper/ScrapperService.cs
@@ -0,0 +1,51 @@
+using FutSpect.Scrapper.Services;
+using FutSpect.Shared.Models;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Playwright;
+
+namespace FutSpect.Scrapper;
+
+public class ScrapperService : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("https://mlssoccer.com/clubs");
+
+        var clubLogos = await page.Locator(".mls-o-clubs-hub-clubs-list__club-logo").AllAsync();
+
+        var clubInfoTasks = clubLogos.Select(GetClubInfo);
+
+        var results = await Task.WhenAll(clubInfoTasks);
+        var clubInfo = results
+            .Where(x => x is not null)
+            .ToList();
+
+        Console.ReadLine();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    static async Task<ClubInfo?> GetClubInfo(ILocator locator)
+    {
+        var imageElement = locator.Locator("picture").Locator("img");
+        var image = await imageElement.GetAttributeAsync("src");
+        var name = await imageElement.GetAttributeAsync("alt");
+
+        if (string.IsNullOrWhiteSpace(image) || string.IsNullOrWhiteSpace(name))
+        {
+            return null;
+        }
+
+        var (Bytes, Extension) = await ImageDownloaderService.DownloadImageAsync(image);
+
+        var clubLogo = new ClubLogo(image, Bytes, Extension);
+        return new ClubInfo(name, clubLogo);
+    }
+}
