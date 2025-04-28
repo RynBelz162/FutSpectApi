5eba2a7a2d4342a57ec9a8b29d38d20681b3359c
RynBelz162 <rynbelz162@gmail.com>
2025-04-28 18:59:51 -0400

    ✨ add file extension column

---
+++ FutSpect.DAL/Migrations/20250427213406_ClubLogoExtensionUpdate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FutSpect.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClubLogoExtensionUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Extension",
+                table: "ClubLogos",
+                type: "character varying(25)",
+                unicode: false,
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScrapeLedger_LeagueId",
+                table: "ScrapeLedger",
+                column: "LeagueId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ScrapeLedger_Leagues_LeagueId",
+                table: "ScrapeLedger",
+                column: "LeagueId",
+                principalTable: "Leagues",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ScrapeLedger_Leagues_LeagueId",
+                table: "ScrapeLedger");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ScrapeLedger_LeagueId",
+                table: "ScrapeLedger");
+
+            migrationBuilder.DropColumn(
+                name: "Extension",
+                table: "ClubLogos");
+        }
+    }
+}
